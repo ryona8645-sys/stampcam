eab6b140e760939abaf7d2bd32b19a26fce69a49
ryona8645-sys <ryona8645@gmail.com>
2026-01-17 22:01:57 +0900

    Update app.js

--- test/app/app.js
+++ test/app/app.js
@@ -1,3 +1,10 @@
-console.log("app.js loaded");
-document.getElementById("btnExport").addEventListener("click", () => alert("clicked"));
+import { exportProjectZipTest } from "./export.js";
 
+console.log("app.js loaded");
+
+document.getElementById("btnExport").addEventListener("click", async () => {
+  console.log("export start");
+  await exportProjectZipTest();
+  console.log("export done");
+  alert("ZIPを作成しました");
+});
