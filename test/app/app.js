8ac07d997ef8fd046a56dfa8f1588211a154a058
ryona8645-sys <ryona8645@gmail.com>
2026-01-17 21:57:22 +0900

    Update app.js

--- test/app/app.js
+++ test/app/app.js
@@ -1,12 +1,3 @@
-import { exportProjectZipTest } from "./export.js";
-
-// Service Worker 登録（PWA化の必須）
-if ("serviceWorker" in navigator) {
-  navigator.serviceWorker.register("./sw.js");
-}
-
-document.getElementById("btnExport").addEventListener("click", async () => {
-  await exportProjectZipTest();
-  alert("ZIPをダウンロードしました（テスト）");
-});
+console.log("app.js loaded");
+document.getElementById("btnExport").addEventListener("click", () => alert("clicked"));
 
